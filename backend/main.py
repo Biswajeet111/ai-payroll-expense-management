108f055edc3a80cb4a660041f5e9a6a2f2ba9ad7
Biswajeet kumar <biswajeetk497@gmail.com>
2026-02-14 04:25:32 +0530

    .

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "https://ai-payroll-expense-management-flax.vercel.app"],
+    "https://ai-payroll-expense-management-5dumfaatp.vercel.app"],
 
     allow_credentials=True,
     allow_methods=["*"],
