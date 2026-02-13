bbc9f585ab011415430504e5a77177ac885422d4
Biswajeet kumar <biswajeetk497@gmail.com>
2026-02-13 23:34:21 +0530

    Initial backend and frontend setup with FastAPI and React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+function App() {
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/")
+      .then(res => console.log(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>AI Payroll Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
